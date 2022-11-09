b800f27d5c127b0efffe7d45039583f230e10583
Aykan Seper <96862833+bloodmoonman@users.noreply.github.com>
2022-11-09 19:39:42 +0300

    Update analyzing_hotel_database.py

--- analyzing_hotel_database.py
+++ analyzing_hotel_database.py
@@ -3,13 +3,12 @@ import sqlite3
 con = sqlite3.connect('hotel_booking.db')
 cur = con.cursor()
 first_row = cur.execute('''SELECT * FROM booking_summary''').fetchone()
-#print(first_row) #FIRST CUSTOMER
+
 first_ten_row = cur.execute('''SELECT * FROM booking_summary''').fetchmany(10) #GETTING THE FIRST 10 CUSTOMER
-#print(first_ten_row)
+
 bra = cur.execute('''SELECT * FROM booking_summary WHERE country = 'BRA';''').fetchall() #GETTING BRAZILIAN CUSTOMERS
 print(bra)
-#print(type(bra))
-#print(len(bra)) JUST CHECKING
+
 
 cur.execute('''CREATE TABLE IF NOT EXISTS bra_customers (
                 num INTEGER,
@@ -31,32 +30,29 @@ first_ten = cur.execute('''SELECT * FROM bra_customers''').fetchmany(10) #THIS S
 print(first_ten)
 
 all_bra = cur.execute('''SELECT * FROM bra_customers''').fetchall()
-#print(all_bra)
-lead_time_can = cur.execute('''SELECT lead_time FROM bra_customers WHERE is_cancelled = 1;''').fetchall() #THIS * MEANS ALL, SO SELECTS ALL OF THE FIELDS NOW IN THE NEXT SS YOU'LL SEE WHAT I MEAN
-#SO NOW I CHANGED THE * TO lead_time SO I WOULD ONLY GET THE lead_time VALUES
-print("--")
-#print(lead_time_can)
+
+lead_time_can = cur.execute('''SELECT lead_time FROM bra_customers WHERE is_cancelled = 1;''').fetchall() 
+
 total = 0
 for i in lead_time_can:
   total += i[0]
   average = total/len(lead_time_can)
 
-print(average)
+
 
 lead_time_not_can = cur.execute('''SELECT lead_time FROM bra_customers WHERE is_cancelled = 0;''').fetchall()
 
-#print(lead_time_not_can)
 not_can_total = 0
 for i in lead_time_not_can:
   not_can_total += i[0]
   average_not_can = not_can_total/len(lead_time_not_can)
 
-print(average_not_can)
+
 
 
 spec_requests = cur.execute('''SELECT special_requests FROM bra_customers WHERE is_cancelled = 1;''').fetchall()
-#WITHOUT FETCHALL() PRINT(SPEC_REQUESTS) RETURNS AN <sqlite3.Cursor object at 0x7fe477c17ea0>
-#print(spec_requests)
+
+
 total_spec_req = 0
 for i in spec_requests:
   total_spec_req += i[0]
